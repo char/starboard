2b6cc3fd6813278f193ded9de5020b91aa45d4f0
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-06-06 05:31:34 +0000

    Production-ready-ify: Add a dockerfile and make rebuild.sh compatible with docker volumes

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
-python3 -m sipy
-rm -rf $STARBOARD_STATIC > /dev/null || true
-cp -r static_out $STARBOARD_STATIC
+if [ ! -z "$STARBOARD_STATIC" ]
+then
+  python3 -m sipy
+  rm -r $STARBOARD_STATIC/*
+  cp -r static_out/. $STARBOARD_STATIC
+fi
