eb736c4da1fa28d88b3601c917ee56a5038b52c6
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-06-07 08:43:04 +0100

    Missed a 'STARBOARD_ADMIN' usage

--- starboard/building.py
+++ starboard/building.py
@@ -6,6 +6,5 @@ import os
 def rebuild_site():
   subprocess.Popen(["./rebuild.sh"], env={
     "STARBOARD_STATIC": STARBOARD_STATIC,
-    "STARBOARD_DATABASE": STARBOARD_DATABASE,
-    "STARBOARD_ADMIN": os.environ.get("STARBOARD_ADMIN")
+    "STARBOARD_DATABASE": STARBOARD_DATABASE
   })
